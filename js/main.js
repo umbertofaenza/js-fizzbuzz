6336d508cb055ea5a49023f61df79345aab656c0
umbertofaenza <umberto.faenza@gmail.com>
2023-07-21 16:28:46 +0200

    feat_js: bonus 1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
+const domContainerEl = document.getElementById("container");
+const unorderedListEL = document.createElement("ul");
+domContainerEl.append(unorderedListEL);
+
 for (let i = 1; i <= 100; i++) {
+  const listItem = document.createElement("li");
+  unorderedListEL.append(listItem);
+
   if (i % 5 == 0 && i % 3 == 0) {
     console.log("FizzBuzz");
+    listItem.innerHTML = "FizzBuzz";
   } else if (i % 3 == 0) {
     console.log("Fizz");
+    listItem.innerHTML = "Fizz";
   } else if (i % 5 == 0) {
     console.log("Buzz");
+    listItem.innerHTML = "Buzz";
   } else {
     console.log(i);
+    listItem.innerHTML = i;
   }
 }
