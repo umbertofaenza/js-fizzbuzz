7dfeab872dcd82de4abe937ffd567fe1504129cd
umbertofaenza <umberto.faenza@gmail.com>
2023-07-21 16:25:44 +0200

    fix_js: fix for cycle

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-for (i = 1; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
   if (i % 5 == 0 && i % 3 == 0) {
     console.log("FizzBuzz");
-    i++;
   } else if (i % 3 == 0) {
     console.log("Fizz");
-    i++;
   } else if (i % 5 == 0) {
     console.log("Buzz");
-    i++;
+  } else {
+    console.log(i);
   }
-  console.log(i);
 }
