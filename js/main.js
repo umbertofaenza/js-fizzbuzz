6bd43d252aeaeca0cb43f35d129b1363959da75d
umbertofaenza <umberto.faenza@gmail.com>
2023-07-21 17:28:33 +0200

    js add (for previous push)

--- js/main.js
+++ js/main.js
@@ -6,20 +6,23 @@ for (let i = 1; i <= 100; i++) {
   const valueBoxEl = document.createElement("div");
   colEl.append(valueBoxEl);
 
+  colEl.classList.add("col");
+  valueBoxEl.classList.add("value-box");
+
   if (i % 5 == 0 && i % 3 == 0) {
     console.log("FizzBuzz");
     valueBoxEl.innerHTML = "FizzBuzz";
+    valueBoxEl.classList.add("fizz-buzz");
   } else if (i % 3 == 0) {
     console.log("Fizz");
     valueBoxEl.innerHTML = "Fizz";
+    valueBoxEl.classList.add("fizz");
   } else if (i % 5 == 0) {
     console.log("Buzz");
     valueBoxEl.innerHTML = "Buzz";
+    valueBoxEl.classList.add("buzz");
   } else {
     console.log(i);
     valueBoxEl.innerHTML = i;
   }
-
-  colEl.classList.add("col");
-  valueBoxEl.classList.add("value-box");
 }
