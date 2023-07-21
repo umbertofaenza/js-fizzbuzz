a56e8be50c0ee3bd01847dd80ee95da219cd84ae
umbertofaenza <umberto.faenza@gmail.com>
2023-07-21 15:42:00 +0200

    feat_js: add FizzBuzz cycle

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 for (i = 1; i <= 100; i++) {
+  if (i % 5 == 0 && i % 3 == 0) {
+    console.log("FizzBuzz");
+    i++;
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+    i++;
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+    i++;
+  }
   console.log(i);
 }
